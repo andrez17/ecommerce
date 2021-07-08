f30f6fb4326fcbe925076f16a5981b195cd4b5d2
Pablo Avecilla <pablo.avecilla@codemettle.com>
2021-07-08 16:17:07 -0400

    created rewards page

--- src/RewardsPage.js
+++ src/RewardsPage.js
@@ -3,7 +3,24 @@ import React from 'react'
 export const RewardsPage = () => {
     return (
         <div className="rewardpage-container">
-            <h1>Reward Page</h1>
+            <div className="rewards-signup">
+                <h1>Sign-In</h1>
+                <form className="rewards-form">
+                    <input value="Email"/>
+                    <input value="Password"/>
+                    <button>SIGN IN</button>
+                </form>
+            </div>
+            <div className="rewards-signup">
+                <h1>Sign-Up</h1>
+                <form className="rewards-form">
+                    
+                    <input value="Email"/>
+                    <input value="New Password"/>
+                    <input value="Confirm Password"/>
+                    <button>SUBMIT</button>
+                </form>
+            </div>
         </div>
     )
 }
