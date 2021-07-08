b3ebb22cbee2de682cb911c10b6a798db0f9ae55
Pablo Avecilla <pablo.avecilla@codemettle.com>
2021-07-08 16:51:50 -0400

    created partners page

--- src/PartnersPage.js
+++ src/PartnersPage.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const PartnersPage = () => {
     return (
         <div className="partnerpage-container">
-            <h1>Partner Page</h1>
+            <div className="mission-statement">
+                <h1>Trusted Experience, Worldwide.</h1>
+                <p>quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione</p>
+            </div>
+            <img src="https://www.unitedwaysei.org/sites/unitedwaysei.org/files/inline-images/Corporate-Partners_Image_List_1020.jpg"/>
         </div>
     )
 }
